9f830c57f52c12831cced3938a8a852dc9d38fb4
Sudam Shaluka <rkshaluka@gmail.com>
2024-01-12 22:12:40 +0530

    Include a database script and provide the SingleConnectionDataSource class along with its corresponding test suite

---
+++ src/test/java/lk/ijse/dep11/pos/db/SingleConnectionDataSourceTest.java
@@ -0,0 +1,21 @@
+package lk.ijse.dep11.pos.db;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SingleConnectionDataSourceTest {
+    @Test
+    void getConnection() {
+        assertDoesNotThrow(SingleConnectionDataSource.getInstance()::getConnection);
+    }
+
+    @Test
+    void getInstance() {
+        var instance1 = SingleConnectionDataSource.getInstance();
+        var instance2 = SingleConnectionDataSource.getInstance();
+        var instance3 = SingleConnectionDataSource.getInstance();
+        assertEquals(instance1, instance2);
+        assertEquals(instance2, instance3);
+    }
+}
